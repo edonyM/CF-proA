57e4fdeba291247e5a986018c45e2f3e3ad65996
edonyM <edonyzpc@gmail.com>
2014-11-10 15:21:46 +0800

    change for the time limits

--- 483A/pro483.cpp
+++ 483A/pro483.cpp
@@ -10,7 +10,7 @@ int main()
     cin>>l>>r;
     for(long i=l+2;i<=r;i++)
     {
-        if(is_coprime(l,i)==0)
+        if(is_coprime(l,i)>1)
         {
             cout<<l<<" "<<l+1<<" "<<i<<endl;
             flag++;
@@ -24,14 +24,18 @@ int main()
 
 int is_coprime(long a,long b)
 {
-    long tmp_min=a<b?a:b;
-    long tmp_max=a>b?a:b;
-    while(tmp_max%tmp_min!=0)
+    long tmp;
+    if(a>b)
     {
-        long tmp=tmp_max%tmp_min;
-        tmp_max=tmp_min;
-        tmp_min=tmp;
+        tmp=a;
+        a=b;
+        b=tmp;
     }
-    if(tmp_min>1)return 0;
-    else return 1;
+    while(b%a!=0)
+    {
+        tmp=b%a;
+        b=a;
+        a=tmp;
+    }
+    return a;
 }
